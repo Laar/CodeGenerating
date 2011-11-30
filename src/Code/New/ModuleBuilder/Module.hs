be3bbe2c440e7b7b29d1401c043efaec9a89e7f6
Lars Corbijn <aspergesoep@hotmail.com>
2011-11-30 21:47:10 +0100

    Changes addImport for module to merge entries.
    This update tries to merge import entries for a module when one is added
    through addImport. The strategy is that entries that have the same 
    parameters (qualified, as, etc.) are merged according to the following plan:
    - Either one imports everything => drop the other (even a hiding one)
    - Both are of the same type (hiding, partial) => combine them.
    - Otherwise => fail to merge and add both (or try to merge with another one)

--- src/Code/New/ModuleBuilder/Module.hs
+++ src/Code/New/ModuleBuilder/Module.hs
@@ -19,7 +19,6 @@ module Code.New.ModuleBuilder.Module (
 
 import Data.List(find)
 import Language.Haskell.Exts.Syntax
-import Code.Utils
 
 import Code.New.ModuleBuilder.Class
 
@@ -31,7 +30,27 @@ instance BuildableModule Module where
                 Just es -> Just $ e : es
                 Nothing -> Nothing -- TODO what should be done here?
     addBDecls d  (Module sl mn mp wt exs im dc) = Module sl mn mp wt exs im (d ++ dc)
-    addBImport i (Module sl mn mp wt exs im dc) = Module sl mn mp wt exs (i:im) dc -- Can we merge?
+    addBImport i (Module sl mn mp wt exs im dc) = Module sl mn mp wt exs (addImportD i im) dc
     addBPragma p (Module sl mn mp wt exs im dc) = Module sl mn (p:mp) wt exs im dc
     getBImport i (Module _  _  _  _  _   im _ ) = find (\im' -> importModule im' == i) im
     hasBPragma p (Module _  _  mp _  _   _  _ ) = p `elem` mp -- TODO is == defined right for this
+
+addImportD :: ImportDecl -> [ImportDecl] -> [ImportDecl]
+addImportD i = go
+    where
+        go []     = [i]
+        go (j:js) = case tryMergeDecls i j of
+            Nothing -> j : go js
+            Just i' -> i' : js
+
+
+tryMergeDecls :: ImportDecl -> ImportDecl -> Maybe ImportDecl
+tryMergeDecls (ImportDecl _ _ _ s _ _ _ ) _ | s = Nothing
+tryMergeDecls _ (ImportDecl _ _ _ s _ _ _ ) | s = Nothing
+tryMergeDecls (ImportDecl _ mn1 q1 _ p1 a1 _) (ImportDecl _ mn2 q2 _ p2 a2 _)
+    | mn1 /= mn2 || a1 /= a2 || q1 /= q2 || p1 /= p2 = Nothing
+tryMergeDecls i@(ImportDecl _ _ _ _ _ _ Nothing) _ = Just i
+tryMergeDecls _ i@(ImportDecl _ _ _ _ _ _ Nothing) = Just i
+tryMergeDecls (ImportDecl l mn q s p a (Just (h1, sp1))) (ImportDecl _ _ _ _ _ _ (Just (h2, sp2)))
+    | h1 == h2  = Just $ ImportDecl l mn q s p a (Just (h1, sp1 ++ sp2))
+    | otherwise = Nothing -- Just to be sure
